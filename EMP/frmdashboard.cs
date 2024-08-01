81fc2419c15b13b1a7cd41721ff3d13bfc6cfe21
azharerode <azharerode@gmail.com>
2024-07-31 23:27:44 -0500

    Requested Breaks by Max_Break_Entry

--- EMP/frmdashboard.cs
+++ EMP/frmdashboard.cs
@@ -20,6 +20,8 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;     
+using System.Data.SqlClient;
 
 namespace EMP
 {
@@ -39,10 +41,6 @@ namespace EMP
 
         public List<string> addlist = new List<string>();
 
-        //0 none
-        //1 punch
-        //2 break
-        //3 out
         private void frmdashboard_Load(object sender, EventArgs e)
         {
             pnllogin.Location = new Point() { X = 12, Y = 12 };
@@ -52,23 +50,6 @@ namespace EMP
             timer3.Start();
             startup();
         }
-
-        //private void ExitMenuItem_Click(object sender, EventArgs e)
-        //{
-        //    DialogResult result = MessageBox.Show("Do you want to punch out before exiting?", "Punch Out Confirmation", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-        //    if (result == DialogResult.Yes)
-        //    {
-        //        punchout();
-        //    }
-        //    else if (result == DialogResult.No)
-        //    {
-        //        Application.Exit(); 
-        //    }
-        //    else
-        //    {
-        //        // If Cancel is selected, do nothing, keeping the application in the tray
-        //    }
-        //}
 
         private void frmdashboard_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -113,8 +94,7 @@ namespace EMP
                 mynotifyicon.ShowBalloonTip(500);
                 this.Hide();
             }
-
-            else if (FormWindowState.Normal == this.WindowState)
+                        else if (FormWindowState.Normal == this.WindowState)
             {
                 mynotifyicon.Visible = false;
             }
@@ -225,20 +205,16 @@ namespace EMP
                 Program.token = objresult.token;
                 pnllogin.Visible = false;
                 loginprocesss();
-
-            }
+                            }
             else
             {
-
-                Logger.LogError("Login Error : Code : " + HttpStatusCode.BadRequest.ToString());
+                                Logger.LogError("Login Error : Code : " + HttpStatusCode.BadRequest.ToString());
                 Logger.LogError(responseString1B);
                 if (errorshow == true)
                 {
                     MessageBox.Show(objresult.message, "", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
-                //var objmaster = JsonConvert.DeserializeObject<ErrorMsg>(responseString1B);
-                //Logger.LogError("Login Server Error");
-                //Logger.LogError(objmaster.Message);
+                
             }
         }
         public void logoutcheck(bool errorshow)
@@ -329,10 +305,7 @@ namespace EMP
             {
                 Logger.LogError("Login Error : Code : " + HttpStatusCode.BadRequest.ToString());
                 Logger.LogError(responseString1B);
-                //var objmaster = JsonConvert.DeserializeObject<ErrorMsg>(responseString1B);
-                //Logger.LogError("Login Server Error");
-                //Logger.LogError(objmaster.Message);
-            }
+                            }
         }
         public void punchout()
         {
@@ -431,25 +404,37 @@ namespace EMP
             }
         }
 
-      
-        private int GetMaxBreakTime(int breakEntryId)
+
+private int GetMaxBreakTime(int breakEntryId)
+    {
+        string connectionString = "Data Source=DESKTOP-M2LCTQE\\SQLEXPRESS;Initial Catalog=EMP4;User Id=sa;Password=Hublog123;Encrypt=True;TrustServerCertificate=True;MultipleActiveResultSets=True;";
+
+        string query = "SELECT Max_Break_Time FROM BreakMaster WHERE Id = @BreakEntryId";
+
+        try
         {
-         
-            if (breakEntryId == 1)
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                return 31; 
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@BreakEntryId", breakEntryId);
+                    connection.Open();
+                    object result = command.ExecuteScalar();
+                    if (result != null && int.TryParse(result.ToString(), out int maxBreakTime))
+                    {
+                        return maxBreakTime;
+                    }
+                }
             }
-            else if (breakEntryId == 2)
-            {
-                return 45; 
-            }
-            else if (breakEntryId == 3)
-            {
-                return 15;
-            }
-            return 0;
         }
-        public void PunchBreakOut(int breakEntryId)
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        return 0;
+    }
+
+    public void PunchBreakOut(int breakEntryId)
         {
             List<UserBreakModel> obj = new List<UserBreakModel>();
             UserBreakModel breakModel = new UserBreakModel
@@ -493,146 +478,7 @@ namespace EMP
                 }
             }
         }
-
-        #region  commented old uploadscreenshot
-        //public void uploadscreenshot(string path)
-        //{
-        //    LoginModels LM = new LoginModels();
-        //    LM.UserName = txtusername.Text;
-        //    LM.Password = txtpassword.Text;
-        //    string filename = Path.GetFileName(path);
-        //    Image im = Image.FromFile(path);
-        //    byte[] imagedata = null;
-        //    using (var ms = new MemoryStream())
-        //    {
-        //        im.Save(ms, im.RawFormat);
-        //        imagedata = ms.ToArray();
-        //    }
-        //    string master = JsonConvert.SerializeObject(LM);
-        //    string URL = Program.OnlineURL + "api/Users/UploadFile";
-        //    string DATA = master;
-        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-        //    System.Net.Http.HttpClient client1B = new System.Net.Http.HttpClient();
-        //    client1B.BaseAddress = new System.Uri(URL);
-        //    client1B.Timeout = TimeSpan.FromMinutes(30);
-        //    client1B.DefaultRequestHeaders.Add("UId", Program.Loginlist.Id.ToString());
-        //    client1B.DefaultRequestHeaders.Add("OId", Program.Loginlist.OrganizationId.ToString());
-        //    client1B.DefaultRequestHeaders.Add("SDate", DateTime.Now.ToString());
-        //    client1B.DefaultRequestHeaders.Add("SType", "ScreenShots");
-        //    client1B.DefaultRequestHeaders.Add("Authorization", Program.token);
-        //    client1B.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        //    System.Net.Http.HttpContent content1B = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
-        //    MultipartFormDataContent content1A = new MultipartFormDataContent();
-        //    content1A.Add(new StreamContent(new MemoryStream(imagedata)), "MyImages", filename);
-        //    HttpResponseMessage messge1B = client1B.PostAsync(URL, content1A).Result;
-        //    var responseString1B = messge1B.Content.ReadAsStringAsync().Result;
-        //    im.Dispose();
-        //    if (messge1B.IsSuccessStatusCode)
-        //    {
-        //        Lastsync = DateTime.Now;
-        //        File.Delete(path);
-        //        addlist.Remove(path);
-        //    }
-        //    else
-        //    {
-        //        if (addlist.Where(c => c == path).Count() == 0)
-        //        {
-        //            addlist.Add(path);
-        //        }
-        //        Logger.LogError("Upload Error : \n " + messge1B + " \n Code : " + HttpStatusCode.BadRequest.ToString());
-        //        Logger.LogError(responseString1B);
-        //    }
-        //}
-        #endregion
-
-        #region  second commented Upload Screenshots
-        //public void uploadscreenshot(string path)
-        //{
-        //    LoginModels LM = new LoginModels
-        //    {
-        //        UserName = txtusername.Text,
-        //        Password = txtpassword.Text
-        //    };
-
-        //    string filename = Path.GetFileName(path);
-        //    Image im = Image.FromFile(path);
-        //    byte[] imagedata;
-        //    using (var ms = new MemoryStream())
-        //    {
-        //        im.Save(ms, im.RawFormat);
-        //        imagedata = ms.ToArray();
-        //    }
-
-        //    string URL = Program.OnlineURL + "api/Users/UploadFile";
-        //    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-
-        //    using (var client1B = new HttpClient())
-        //    {
-        //        client1B.BaseAddress = new Uri(URL);
-        //        client1B.Timeout = TimeSpan.FromMinutes(30);
-        //        client1B.DefaultRequestHeaders.Add("UId", Program.Loginlist.Id.ToString());
-        //        client1B.DefaultRequestHeaders.Add("OId", Program.Loginlist.OrganizationId.ToString());
-        //        client1B.DefaultRequestHeaders.Add("SDate", DateTime.Now.ToString());
-        //        client1B.DefaultRequestHeaders.Add("SType", "ScreenShots");
-        //        client1B.DefaultRequestHeaders.Add("Authorization", Program.token);
-        //        client1B.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-        //        var content1A = new MultipartFormDataContent();
-
-        //        var imageContent = new ByteArrayContent(imagedata);
-        //        imageContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
-        //        content1A.Add(imageContent, "MyImages", filename);
-
-        //        HttpResponseMessage responseMessage = client1B.PostAsync(URL, content1A).Result;
-        //        string responseString = responseMessage.Content.ReadAsStringAsync().Result;
-
-        //        im.Dispose();
-
-        //        if (responseMessage.IsSuccessStatusCode)
-        //        {
-        //            Lastsync = DateTime.Now;
-        //            File.Delete(path);
-        //            addlist.Remove(path);
-        //        }
-        //        else
-        //        {
-        //            if (!addlist.Contains(path))
-        //            {
-        //                addlist.Add(path);
-        //            }
-        //            Logger.LogError("Upload Error : \n " + responseMessage + " \n Code : " + HttpStatusCode.BadRequest.ToString());
-        //            Logger.LogError(responseString);
-        //        }
-        //    }
-        //}
-        #endregion
-
-        #region  second commented screenshot
-        //public void screenshot()
-        //{
-        //    Rectangle bounds = Screen.GetBounds(Point.Empty);
-        //    string name = DateTime.Now.ToString("yyyyMMddHHmmss");
-        //    name = Application.StartupPath + "\\ScreenShot\\" + name + ".jpg";
-        //    using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
-        //    {
-        //        using (Graphics g = Graphics.FromImage(bitmap))
-        //        {
-        //            g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-        //        }
-        //        bitmap.Save(name, ImageFormat.Jpeg);
-        //    }
-        //    addlist.Add(name);
-
-        //    if (timer2.Enabled == false)
-        //    {
-        //        timer2.Start();
-        //    }
-        //}
-        #endregion
-
-
-
-        public void screenshot()
+public void screenshot()
         {
             Rectangle bounds = Screen.GetBounds(Point.Empty);
             using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
@@ -701,80 +547,6 @@ namespace EMP
                 }
             }
         }
-
-        #region  Commented Screenshot Interval from API
-        //private readonly object listLock = new object();
-        //private async Task screenshot() 
-        //{
-        //    int interval = await GetScreenshotIntervalAsync();
-
-        //    timer1.Interval = interval;
-
-        //    Rectangle bounds = Screen.GetBounds(Point.Empty);
-        //    string name = DateTime.Now.ToString("yyyyMMddHHmmss");
-        //    string directoryPath = Path.Combine(Application.StartupPath, "ScreenShot");
-
-        //    if (!Directory.Exists(directoryPath))
-        //    {
-        //        Directory.CreateDirectory(directoryPath);
-        //    }
-
-        //    string filePath = Path.Combine(directoryPath, $"{name}.jpg");
-
-        //    using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
-        //    {
-        //        using (Graphics g = Graphics.FromImage(bitmap))
-        //        {
-        //            g.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
-        //        }
-        //        bitmap.Save(filePath, ImageFormat.Jpeg);
-        //    }
-
-        //    lock (listLock)
-        //    {
-        //        addlist.Add(filePath);
-        //    }
-
-        //    if (!timer2.Enabled)
-        //    {
-        //        timer2.Interval = interval;
-        //        timer2.Start();
-        //    }
-        //    else
-        //    {
-        //        timer2.Interval = interval;
-        //    }
-        //}
-
-        //private async Task<int> GetScreenshotIntervalAsync()
-        //{
-        //    using (HttpClient client = new HttpClient())
-        //    {
-        //        HttpResponseMessage response = await client.GetAsync(Program.OnlineURL + "api/Screenshot/GetSelectedInterval");
-        //        response.EnsureSuccessStatusCode();
-
-        //        string responseBody = await response.Content.ReadAsStringAsync();
-        //        Console.WriteLine($"Response Body: {responseBody}");
-
-        //        try
-        //        {
-        //            var intervals = JArray.Parse(responseBody);
-        //            if (intervals.Count > 0)
-        //            {
-        //                var intervalInMilliseconds = (int)intervals[0]["intervalInMilliseconds"];
-        //                return intervalInMilliseconds;
-        //            }
-
-        //            throw new Exception("No intervals found in API response.");
-        //        }
-        //        catch (JsonException ex)
-        //        {
-        //            throw new Exception("Error parsing JSON response.", ex);
-        //        }
-        //    }
-        //}
-        #endregion
-
         public void changestatus()
         {
             if (currenttype == 0)
@@ -814,60 +586,6 @@ namespace EMP
                 timer1.Start();
             }
         }
-
-
-        #region=======Timers==============
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            screenshot();
-            //browserdetails();
-        }
-        //private void timer2_Tick(object sender, EventArgs e)
-        //{
-        //    if (addlist.Count != 0)
-        //    {
-        //        for (int i = 0; i < addlist.Count; i++)
-        //        {
-        //            uploadscreenshot(addlist[i]);
-        //        }
-        //    }
-        //}
-
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            if (addlist.Count != 0)
-            {
-                for (int i = 0; i < addlist.Count; i++)
-                {
-                    string filePath = addlist[i];
-
-                    byte[] imagedata = File.ReadAllBytes(filePath);
-
-                    uploadscreenshot(imagedata);
-                }
-            }
-        }
-
-        private void timer3_Tick(object sender, EventArgs e)
-        {
-            lblcurrenttime.Text = DateTime.Now.ToString("ddd dd MMMM yyyy HH:mm:ss");
-            TimeSpan ts = SW.Elapsed;
-            lbltimer.Text = String.Format("{0:00}:{1:00}:{2:00}",
-            ts.Hours, ts.Minutes, ts.Seconds);
-            TimeSpan sync = (Lastsync - DateTime.Now);
-            if (sync.TotalMinutes > 60)
-            {
-                diff = String.Format("{1} hours {2} minutes", sync.Hours, sync.Minutes);
-            }
-            else
-            {
-                diff = sync.Minutes + " mins";
-            }
-
-            lbllastsync.Text = "Last Sync " + diff + " Ago";
-        }
-        #endregion
-
         private void btnlogout_Click(object sender, EventArgs e)
         {
             logoutcheck(true);
