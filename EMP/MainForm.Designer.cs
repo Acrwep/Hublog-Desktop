54dac76350bb3322a92fc29f2eec3790f78c89e8
azharerode <azharerode@gmail.com>
2024-08-09 22:27:59 -0500

    Savetimer

---
+++ EMP/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Hublog
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(397, 88);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(35, 13);
+            this.lblTimer.TabIndex = 0;
+            this.lblTimer.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTimer);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTimer;
+    }
+}
---
+++ EMP/MainForm.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Hublog
+{
+    public partial class MainForm : Form
+    {
+        private DateTime targetTime;
+        private Timer timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
+            LoadTimerState();
+            SetupTimer();
+        }
+
+        private void SetupTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 1000; 
+            timer.Tick += Timer_Tick;
+            UpdateTimerUI();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now >= targetTime)
+            {
+                timer.Stop();
+                TimerFinished();
+            }
+            else
+            {
+                UpdateTimerUI();
+            }
+        }
+
+        private void UpdateTimerUI()
+        {
+            TimeSpan remainingTime = targetTime - DateTime.Now;
+            lblTimer.Text = remainingTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void SaveTimerState()
+        {
+            TimeSpan remainingTime = targetTime - DateTime.Now;
+            File.WriteAllText("timerState.txt", remainingTime.ToString());
+        }
+
+        private void LoadTimerState()
+        {
+            if (File.Exists("timerState.txt"))
+            {
+                string remainingTimeStr = File.ReadAllText("timerState.txt");
+                if (TimeSpan.TryParse(remainingTimeStr, out TimeSpan remainingTime))
+                {
+                    targetTime = DateTime.Now.Add(remainingTime);
+                    UpdateTimerUI();
+                    timer.Start();
+                }
+            }
+        }
+
+        private void CleanUpTimerState()
+        {
+            if (File.Exists("timerState.txt"))
+            {
+                File.Delete("timerState.txt");
+            }
+        }
+
+        private void TimerFinished()
+        {
+            CleanUpTimerState();
+            MessageBox.Show("Time's up!");
+            
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTimerState();
+        }
+
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            SaveTimerState();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            SaveTimerState();
+            base.OnFormClosing(e);
+        }
+    }
+}
